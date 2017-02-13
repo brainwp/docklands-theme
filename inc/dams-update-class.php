a3b5ee9fed65bc053b99024579aeb4e7aa87f4fa
matheusgimenez <contato@matheusgimenez.com.br>
2017-02-13 18:46:51 -0200

    refazendo classe de cron dams

--- inc/dams-update-class.php
+++ inc/dams-update-class.php
@@ -7,88 +7,152 @@ ini_set('max_execution_time', 600);
  * @author Brasa
  */
 class Brasa_Dams_FTP_Update{
+	/**
+	 * @var array|boolean
+	 */
+	private $log_file = false;
+	/**
+	 * construct class
+	 */
 	public function __construct() {
 		add_action('init', array($this,'do_cron'));
 	}
-	public function add_cron(){
-		if( wp_next_scheduled( 'brasa_dams_cron' ) )
-			return;
+	/**
+	 * Get product ID by SKU
+	 * @param string $sku
+	 * @return boolean|int
+	 */
+	private function get_product_by_sku( $sku ) {
+		global $wpdb;
+		$product_id = $wpdb->get_var( $wpdb->prepare( "SELECT post_id FROM $wpdb->postmeta WHERE meta_key='_sku' AND meta_value='%s' LIMIT 1", $sku ) );
 
-		wp_schedule_event( time(), 'hourly', 'brasa_dams_cron' );
+		if ( $product_id ) {
+			return $product_id;
+		}
+
+		return false;
 	}
+	/**
+	 * Get XML file content, load from FTP if not loaded yet
+	 * @return string
+	 */
 	private function get_xml_file(){
 		$cfg = get_option('woo_cfg');
 		if($cfg && $cfg['dams_ftp_host']) {
-			$ftp_connect = ftp_connect( $cfg['dams_ftp_host'] );
-			$login_result = ftp_login( $ftp_connect, $cfg['dams_ftp_user'], $cfg['dams_ftp_pass'] );
-			//$connect = sprintf('ftp://%s:%s@%s/Stock.xml', $cfg['dams_ftp_user'], $cfg['dams_ftp_pass'], $cfg['dams_ftp_host']);
-
-			if ( !$ftp_connect || !$login_result ) {
-    			die( 'login failed' );
-    		}
     		//ftp_chdir( $ftp_connect, '/' );
-    		$local = fopen( get_template_directory() . '/inc/temp.xml', 'w' );
-    		$result = ftp_fget( $ftp_connect, $local, 'Stock.xml', FTP_BINARY );
-			fclose( $local );
-			ftp_close( $ftp_connect );
+    		$file_name = sprintf( '%s.xml', current_time( 'Y-m-d' ) );
+    		$file_location = WP_CONTENT_DIR . '/dams/' . $file_name;
+    		if ( ! file_exists( $file_location ) ) {
+    			if( ! file_exists( WP_CONTENT_DIR . '/dams' ) ) {
+    				mkdir( WP_CONTENT_DIR . '/dams', 0755 );
+    			}
+    			$ftp_connect = ftp_connect( $cfg['dams_ftp_host'] );
+				$login_result = ftp_login( $ftp_connect, $cfg['dams_ftp_user'], $cfg['dams_ftp_pass'] );
+				//$connect = sprintf('ftp://%s:%s@%s/Stock.xml', $cfg['dams_ftp_user'], $cfg['dams_ftp_pass'], $cfg['dams_ftp_host']);
 
-            $content = file_get_contents( get_template_directory() . '/inc/temp.xml' );
+				if ( !$ftp_connect || !$login_result ) {
+    				die( 'login failed' );
+    			}
+
+    			$local = fopen( $file_location, 'w' );
+    			$result = ftp_fget( $ftp_connect, $local, 'Stock.xml', FTP_BINARY );
+				fclose( $local );
+				ftp_close( $ftp_connect );
+    		}
+            $content = file_get_contents( $file_location );
 			return $content;
 		}
 	}
+	/**
+	 * Log messages on a txt file located in wp-content/dams
+	 * @param type $message
+	 * @return type
+	 */
+	private function log( $message ) {
+    	$file_name = sprintf( '%s_log.txt', current_time( 'Y-m-d' ) );
+		$file_location = WP_CONTENT_DIR . '/dams/' . $file_name;
+		if ( ! file_exists( $file_location ) ) {
+    		if( ! file_exists( WP_CONTENT_DIR . '/dams' ) ) {
+    			mkdir( WP_CONTENT_DIR . '/dams', 0755 );
+    		}
+    	}
+    	if ( false === $this->log_file ) {
+    		$this->log_file = fopen( $file_location, 'a+' );
+    	}
+    	fwrite( $this->log_file, $message . "\n" );
+    	echo $message . '<br>';
+	}
+	/**
+	 * Close open files and kill execution
+	 */
+	private function close_cron() {
+		if ( false !== $this->log_file ) {
+			$this->log( "---- STOPPED ---\n" );
+			fclose( $this->log_file );
+		}
+		die();
+	}
+	/**
+	 * Execute Dams Cron
+	 */
 	public function do_cron(){
 		if(is_admin() || !isset( $_GET['do_dams_cron'] ) )
 			return;
 		$update_date = sprintf( 'update_stock_last_date_%s', current_time( 'Y-m-d' ) );
-		$file = simplexml_load_string( $this->get_xml_file() ) ;
-		// WP_Query arguments
-		$args = array (
-			'post_type'              => array( 'product', 'product_variation'),
-			'posts_per_page'		 => 1200,
-			'meta_query'             => array(
-				'relation' => 'AND',
-				array(
-					'key'       => '_sku',
-					'compare'   => 'EXISTS',
-				),
-				array(
-					'key'     => $update_date,
-					'compare' => 'NOT EXISTS',
-				),
-			),
-		);
-
-		// The Query
-		$query = new WP_Query( $args );
-		// The Loop
-
-		if ( $query->have_posts() ) {
-			$i = 0;
-			while ( $query->have_posts() ) {
-				$i++;
-				$query->the_post();
-				$path = sprintf('//Product[@Code="%s"]', get_post_meta( get_the_ID(), '_sku', true));
-				$xml = json_decode( json_encode( (array) $file->xpath($path)), true );
-				if($xml && !empty($xml) && is_array($xml)){
-					$qty = $xml[0]['Warehouse']['@attributes']['Available'];
-					update_post_meta( get_the_ID(), '_stock', $qty);
-					update_post_meta( get_the_ID(), '_manage_stock', 'yes');
-					if(intval($qty) > 0){
-						update_post_meta( get_the_ID(), '_stock_status', 'instock');
-					}
-					else{
-						update_post_meta( get_the_ID(), '_stock_status', 'outofstock');
-					}
-					echo 'foi!<br>';
-				}
-				echo $i;
-				update_post_meta( get_the_ID(), $update_date, 'true' );
-
+		$file = simplexml_load_string( $this->get_xml_file() );
+		$xml_data = (array) json_decode( json_encode( (array) $file ), true );
+		$max_per_load = 1000;
+		$i = 1;
+		$stopped_on = get_option( sprintf( 'dams_stopped_%s', current_time( 'Y-m-d' ) ), 1 );
+		foreach ( $xml_data[ 'Product'] as $product ) {
+			if ( $i < intval( $stopped_on ) ) {
+				continue;
 			}
-			die();
+			if ( $i > $max_per_load ) {
+				break;
+			}
+			$product = ( array )$product;
+			$log_message = sprintf( 'Index[%s]: ', $i );
+			var_dump( $product );
+			if ( ! isset( $product[ '@attributes' ][ 'Code' ] ) ) {
+				$this->log( $log_message . 'não tem atributo Code no XML' );
+				$i++;
+				continue;
+			}
+			if ( ! isset( $product[ '@attributes' ][ 'Code' ] ) ) {
+				$this->log( $log_message . 'não tem atributo Code no XML' );
+				$i++;
+				continue;
+			}
+
+			$log_message = sprintf( 'Index[%s] - Code[%s]: ', $i, $product[ '@attributes' ][ 'Code' ] );
+			$product_id = $this->get_product_by_sku( $product[ '@attributes' ][ 'Code' ] );
+			if ( false === $product_id ) {
+				$this->log( $log_message . 'não foi encontrado produto com esse SKU no banco' );
+				$i++;
+				continue;
+			}
+			$sku = get_post_meta( $product_id, '_sku', true );
+			$this->log( $log_message . 'SKU no banco: ' . $sku );
+			$log_message = sprintf( 'Index[%s] - Code[%s] - WPID[%s]: ', $i, $product[ '@attributes' ][ 'Code' ], $product_id );
+			if ( ! isset( $product['Warehouse']['@attributes']['Available'] ) ) {
+				$this->log( $log_message . 'não tem atributo Available no XML' );
+				$i++;
+				continue;
+			}
+			$qty = $product['Warehouse']['@attributes']['Available'];
+			$qty_old = get_post_meta( $product_id, '_stock', true );
+			update_post_meta( $product_id, '_stock', $qty);
+			update_post_meta( $product_id, '_manage_stock', 'yes');
+			if(intval($qty) > 0){
+				update_post_meta( $product_id, '_stock_status', 'instock');
+			} else {
+				update_post_meta( $product_id, '_stock_status', 'outofstock');
+			}
+			$this->log( $log_message . sprintf( 'Atualizado! {Quantidade anterior: %s} {Nova Quantidade: %s}', $qty_old, $qty ) );
+			$i++;
 		}
-		// Restore original Post Data
-		wp_reset_postdata();
+		$this->close_cron();
 	}
 }
 new Brasa_Dams_FTP_Update();
